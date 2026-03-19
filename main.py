2f41151a804b75623948fd1264276630cb261168
TLado <tordaladomerszky@gmail.com>
2026-03-19 16:49:39 +0100

    a main.py már tud olvasni a room_layout által generált csv-ből

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
+import pandas as pd
 
 # Konstansok
-devices_and_values = {"laptop": 10, "monitor": 9, "printer": 3, "router": 5, "other": 4}
+devices_and_values = {"computer": 9, "monitor": 3, "phone": 5}
 DISTANCE_WHEN_MEASURED = 0.1  # méter, amikor az alapvető értékeket mértük meg az eszközöknél ennyi méterre voltunk
-ROOM_SIZE_X = 20               # méterben
-ROOM_SIZE_Y = 30
+# ROOM_SIZE_X = 20               # méterben
+# ROOM_SIZE_Y = 30
 RES = 0.1                     # felbontás (10 cm), ez a heatmap felbontása
 
+"""
 def data_input():
     # Felhasználótól az eszköz és a koordinátáinak a bekérése
     dev_types = []
@@ -39,11 +41,12 @@ def data_input():
             break
             
     return dev_types, dxs, dys
+"""
 
-def calculate_combined_heatmap(dev_types, dxs, dys):
+def calculate_combined_heatmap(dev_types, dxs, dys, width_m, height_m):
     # Rács létrehozása
-    x = np.arange(0, ROOM_SIZE_X + RES, RES)
-    y = np.arange(0, ROOM_SIZE_Y + RES, RES)
+    x = np.arange(0, width_m + RES, RES)
+    y = np.arange(0, height_m + RES, RES)
     X, Y = np.meshgrid(x, y)
     
     # Kezdeti térerősség mindenhol nulla
@@ -73,14 +76,72 @@ def format_coord(x, y):
         return f'x={x:.2f}, y={y:.2f}, B={z:.4f} \u03bcT'
     return f'x={x:.2f}, y={y:.2f}'
 
+def import_csv(): 
+    room_layout = pd.read_csv("room_layout.csv", sep=";")
+
+    """
+    CSV oszlopainak jelentése: 
+    x           : A cella oszlopindexe a rácsban (egész szám).
+    y           : A cella sorindexe a rácsban (egész szám).
+    x_m         : A cella vízszintes középpontja méterben kifejezve.
+    y_m         : A cella függőleges középpontja méterben kifejezve.
+    cell_size_m : Egy cella oldalhossza méterben (felbontás).
+    is_wall     : Bináris (0/1); 1-es, ha a cella egy fal része.
+    items       : Ez bevallom nem tudom mi (Alexet kéne kérdezni)
+    table       : Bináris (0/1); 1-es, ha asztal található a cellában.
+    computer    : Bináris (0/1); 1-es, ha számítógép található a cellában.
+    monitor     : Bináris (0/1); 1-es, ha monitor található a cellában.
+    phone       : Bináris (0/1); 1-es, ha telefon található a cellában.
+    person      : Bináris (0/1); 1-es, ha személy tartózkodik a cellában.
+
+    """
+
+    dev_types = []
+    dxs = []
+    dys = []
+
+    computers = room_layout[room_layout["computer"] == 1]
+    monitors = room_layout[room_layout["monitor"] == 1]
+    phones = room_layout[room_layout["phone"] == 1]
+
+
+    for item in computers.itertuples():
+        dev_types.append("computer")
+        dxs.append(item.x_m)
+        dys.append(item.y_m)
+
+    for item in monitors.itertuples():
+        dev_types.append("monitor")
+        dxs.append(item.x_m)
+        dys.append(item.y_m)
+
+    for item in phones.itertuples():
+        dev_types.append("phone")
+        dxs.append(item.x_m)
+        dys.append(item.y_m)
+
+    # A legnagyobb koordináták megkeresése
+    max_x_m = room_layout['x_m'].max()
+    max_y_m = room_layout['y_m'].max()
+    cell_size_m = room_layout['cell_size_m'].iloc[0]
+
+    # A szoba szélessége és hossza méterben (még falak nélkül, ezért vonok ki egyet-egyet)
+    width_m = max_x_m + cell_size_m - 1
+    height_m = max_y_m + cell_size_m - 1
+
+
+    return dev_types, dxs, dys, width_m, height_m, cell_size_m
 
 
 if __name__ == "__main__":
     # 1. Adatok bekérése
-    dev_types, dxs, dys = data_input()
+    dev_types, dxs, dys, width_m, height_m, cell_size_m = import_csv()
+    print(dev_types)
+    print(dxs)
+    print(dys)
 
     # 2. Számítás
-    X, Y, Z = calculate_combined_heatmap(dev_types, dxs, dys)
+    X, Y, Z = calculate_combined_heatmap(dev_types, dxs, dys, width_m, height_m)
 
     # 3. Megjelenítés
     plt.figure(figsize=(10, 8))
