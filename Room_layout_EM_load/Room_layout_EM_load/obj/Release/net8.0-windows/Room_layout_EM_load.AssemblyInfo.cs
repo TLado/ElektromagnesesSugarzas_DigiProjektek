be914c27c94d539830b064b8c59dcd143c14dd49
feketealexm <feketealexm@gmail.com>
2026-05-08 17:04:58 +0200

    Telepítő hozzáadva (majd a dokumentációba beírni, hogy ne promragfiles (x86) alá mentsék, mert akkor nem működik

--- Room_layout_EM_load/Room_layout_EM_load/obj/Release/net8.0-windows/Room_layout_EM_load.AssemblyInfo.cs
+++ Room_layout_EM_load/Room_layout_EM_load/obj/Release/net8.0-windows/Room_layout_EM_load.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Room_layout_EM_load")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42bca9b7e489ce40bc677978685a15c2792438f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d0eb8bd2aa97e95e8c910e594b5378e5c6ca5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Room_layout_EM_load")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Room_layout_EM_load")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
